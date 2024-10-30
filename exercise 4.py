f961d604b838d637bcf8cd5d32de9c9ea067cea2
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-10-30 14:21:46 +0400

    Create exercise 4.py

---
+++ exercise 4.py
@@ -0,0 +1,6 @@
+#asking the user to enter data and evaluating them
+b=input("What is the capital of France :")
+if b ="Paris":
+    print("your answer is correct")
+else:
+    print("your answer is incorrect")
