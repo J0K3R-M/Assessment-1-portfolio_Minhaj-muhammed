3115b5dd707c092fb509815ab56a10653c8910f3
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-11-12 13:28:47 +0400

    Update exercise 3.py

--- exercise 3.py
+++ exercise 3.py
@@ -1,7 +1,11 @@
 #working with dictionary 
-M=input('Name':)
-I=input('Home Town':)
-N=int(input('Age':))
-print(M)
-print(I)
-print(N)
+#Storing personal information (Name,Hometown,Age) of an individual in a variable called Personal information
+print()
+Key1={"Name":"Minhaj muhammed"}
+print(Key1)  #Displaying the data stored in the variable Key1
+print()      #Displaying an empty line
+Key2={"Hometown":"Kerala"}
+print(Key2)  #Displaying the data stored in the variable Key2
+print()      #Displaying an empty line 
+Key3={"Age":18}
+print(Key3) #Displaying the data stored in the variable Key3
