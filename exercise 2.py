5bf830f811524989425b64623ef20f901f90dbe7
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-11-12 13:28:42 +0400

    Update exercise 2.py

--- exercise 2.py
+++ exercise 2.py
@@ -1,5 +1,5 @@
 #creating and working with integer variables and performing arithemetic operations
-price=8
-quantity=10
-total=(price+quantity)
-print(total)
+price=8 #Declaring a variable "Price" and inserting an integer value "8" into it
+quantity=10 #Declaring a variable "Quantity" and inserting an integer value "10" into it
+total=(price+quantity)  #Assigned another variable "Total" to store the sum or price and quantity
+print(total) #displaying the sum 
