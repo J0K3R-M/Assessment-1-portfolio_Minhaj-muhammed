b5caa8923fcc8521828ad7e5e4cc1ff313b83664
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-11-12 21:31:14 +0400

    Update exercise 1.py

--- exercise 1.py
+++ exercise 1.py
@@ -4,3 +4,4 @@ word2="is " # The word "Is" being stored into variable "word2"
 word3="cool"# The word "Cool" being stored into variable "word3"
 print(word1+word2+word3) #displaying the code
 
+print("~~THANK YOU~~")
