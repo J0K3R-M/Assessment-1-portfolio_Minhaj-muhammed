8dd88e8368b77f6b2e12b1395212bb9225b2e7a8
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-11-12 13:29:06 +0400

    Delete exercise 4.py

--- exercise 4.py
+++
@@ -1,6 +0,0 @@
-#asking the user to enter data and evaluating them
-b=input("What is the capital of France :")
-if 
-    print("your answer is correct")
-else:
-    print("your answer is incorrect")
