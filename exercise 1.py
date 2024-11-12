33096a7abdfb2ce40deb0b91af09aa3b18443987
Minhaj Muhammed <minhajmuhammed076@gmail.com>
2024-11-12 13:28:07 +0400

    Update exercise 1.py

--- exercise 1.py
+++ exercise 1.py
@@ -1,5 +1,6 @@
 #string concantination
-word1="Coding "
-word2="is "
-word3="cool"
-print(word1+word2+word3)
+word1="Coding " # The word "Coding" being stored into variable "word1"
+word2="is " # The word "Is" being stored into variable "word2"
+word3="cool"# The word "Cool" being stored into variable "word3"
+print(word1+word2+word3) #displaying the code
+
